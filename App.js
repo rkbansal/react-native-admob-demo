be3fa0d778951e7893aaccbc00e5fbcc679dc525
Raj <rajkumarbansal479@gmail.com>
2018-04-17 22:04:14 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,109 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button, ScrollView } from "react-native";
+import {
+  AdMobBanner,
+  AdMobInterstitial,
+  PublisherBanner,
+  AdMobRewarded
+} from "expo";
+
+// const ADUNITID = `ca-app-pub-1425926517331745~6816357585`;
+const BANNER_ID = `ca-app-pub-7052726238814167/4912710981`;
+const INTERSTITIAL_ID = `ca-app-pub-7052726238814167/3617631566`;
+// const REWARDED_ID = `ca-app-pub-1425926517331745/3923257478`;
+
+AdMobInterstitial.setAdUnitID(INTERSTITIAL_ID);
+AdMobInterstitial.setTestDeviceID("EMULATOR");
+// AdMobRewarded.setAdUnitID(REWARDED_ID);
+// console.disableYellowBox = true;
+
+class App extends Component {
+  _openInterstitial = async () => {
+    await AdMobInterstitial.requestAdAsync();
+    await AdMobInterstitial.showAdAsync();
+  };
+  componentDidMount(){
+
+    AdMobInterstitial.addEventListener("interstitialDidLoad",()=>{
+      console.log("interstitialDidOpen");
+    });
+
+    AdMobInterstitial.addEventListener("interstitialDidFailToLoad",()=>{
+      console.log("interstitialDidFailToLoad");
+    });
+
+    AdMobInterstitial.addEventListener("interstitialDidOpen",()=>{
+      console.log("interstitialDidOpen");
+    });
+
+    AdMobInterstitial.addEventListener("interstitialDidClose",()=>{
+      console.log("interstitialDidClose");
+    });
+
+    AdMobInterstitial.addEventListener("interstitialWillLeaveApplication",()=>{
+      console.log("interstitialWillLeaveApplication");
+    });
+  }
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.title}>GOOGLE ADMOB DEMO</Text>
+          <Text style={styles.text}>
+            Set Ad Unit Id, Interstitial Id & Rewarded Id only on the top level
+            component once.
+          </Text>
+          <Text style={styles.title}>BANNER AD</Text>
+          <AdMobBanner
+            bannerSize="fullBanner"
+            adUnitID={BANNER_ID}
+            didFailToReceiveAdWithError={this.bannerError}
+          />
+
+          <Text style={styles.title}>PUBLISHER BANNER</Text>
+          {/* <PublisherBanner
+            bannerSize="banner"
+            adUnitID={BANNER_ID}
+            didFailToReceiveAdWithError={this.bannerError}
+            admobDispatchAppEvent={this.adMobEvent}
+          /> */}
+          <View style={styles.btn}>
+            <Text style={styles.title}>INTERSTITIAL AD</Text>
+            <Button
+              title="OPEN"
+              color="steelblue"
+              onPress={this._openInterstitial}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginTop: 30
+  },
+  btn: {
+    margin: 7
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 24,
+    color: "#000",
+    backgroundColor: "#eee333",
+    padding: 10,
+    margin: 10
+  },
+  title: {
+    marginTop: 10,
+    fontSize: 30,
+    color: "#333"
+  }
+});
+
+export default App;
